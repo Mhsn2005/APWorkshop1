f9630eb8907647ccb0b3a9bae247dafa1302ffc7
Mhsn2005 <jafaripanahazade@gmail.com>
2024-10-07 14:40:37 +0330

    define the fields

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+         Stack<Integer> stack = new Stack<>();
+        int num=0;
+        int sign=1;
+        char operator='+';
+
+    }
+}
