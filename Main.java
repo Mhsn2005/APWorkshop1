35dd94652153631e325c363b104d00479774c0b4
Mhsn2005 <jafaripanahazade@gmail.com>
2024-10-07 20:55:13 +0330

    continuing in coding that released the code can not differ * with + in piority so this is just the last commit for you to see so far i'm gonna start a new algorithm that i just learned in DS class at 3pm

--- Main.java
+++ Main.java
@@ -26,6 +26,23 @@ public class Main {
                      sign=-1;
                  }
             }
+            else if (ch=='(') {
+                stack.push(sign); // to know what was the sign before the parentheses
+                stack.push(null); // to know where our parentheses opened
+                // reset the number and other things for going inside the parentheses
+                sign=1;
+                operator='+';
+                num=0;
+            }
+            else if (ch==')') {
+                op.operate(operator , num , sign , stack); // processing the last function that was in the parentheses
+                num=0; // we want to get outside the parentheses so we should reset the number
+                int innersum=0; // for the result inside the parentheses
+                while (stack.peek() != null) { // we go ahead and process the innersum until we reach the null ( start of the parentheses)
+                    innersum += stack.pop();
+                }
+                stack.pop(); // for pop the null in stack
+            }
         }
 
     }
