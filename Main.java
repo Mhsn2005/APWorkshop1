df0cb1724d2deb281778fa843e1b8a81a845e654
Mhsn2005 <jafaripanahazade@gmail.com>
2024-10-07 14:53:23 +0330

    reading the input but assuming there is no parentheses

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
@@ -6,6 +7,26 @@ public class Main {
         int num=0;
         int sign=1;
         char operator='+';
+        Operations op = new Operations();
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+        for (int i = 0; i < s.length(); i++) {//starting to proccessing the input
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) {
+                num = num * 10 + ch - '0'; // for managing numbers grater than 9
+            }
+            else if (ch=='+' || ch=='-' || ch=='*' || ch=='/') {
+                 op.operate(operator , num , sign , stack); // first we operate the last operation
+                 operator=ch; //now updating the operator and the number to the new ones
+                 num=0;
+                 if(ch=='+'){ // we should update the sign too
+                     sign=1;
+                 }
+                 else if (ch=='-'){
+                     sign=-1;
+                 }
+            }
+        }
 
     }
 }
